b0368536a267857b67d78051973d35adc216b7a3
Artem Lajko <110809333+la-cc@users.noreply.github.com>
2025-11-07 18:48:02 -0500

    sync

--- demo.sh
+++ demo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+code() { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*; }
+
 # setup
 if [ ! -f demo-magic.sh ]; then
     curl -LO https://github.com/paxtonhare/demo-magic/raw/master/demo-magic.sh
@@ -13,35 +15,48 @@ clear
 
 # Demo #1
 pe "echo \"Demo #1 - Developer Focus through Abstraction with Kro\""
-pe "code images/demo-0.png"
-pe "cp podinfo/gen-man-by-dev-podinfo.yaml apps/gen-man-by-dev-podinfo.yaml"
-#pe "./scripts/sync_argocd_apps.sh demo-2"
-pe "code kro/workload.yaml"
-pe "code podinfo/gen-man-by-dev-podinfo.yaml"
+pe "echo \"What is Kro?\""
+pe "code images/demo-6.png"
+pe "code images/demo-0.png" #light version
+pe "code -g kro/workload.yaml:1"
+pe "code podinfo/gen-man-by-dev-podinfo-minimal-setup.yaml"
+pe "code images/demo-0.png" #current version
+cp podinfo/gen-man-by-dev-podinfo.yaml apps/gen-man-by-dev-podinfo.yaml
+pe "echo \"GitOps Deployment - Demo 1\""
+./scripts/sync_argocd_apps.sh demo-1 >/dev/null 2>&1""
+pe "kubectl get workload,all,hpa,netpol,sa -n podinfo-kro"
 
 # Demo #2
 pe "echo \"Demo #2 - Platform Infra Standardization (Redis) with Kro\""
 pe "code images/demo-2-redis-in-cluster.png"
-pe "code podinfo-with-redis/gen-man-by-dev-podinfo-redis.yaml"
+pe "code podinfo-with-redis/gen-man-by-dev-podinfo-minimal-setup.yaml"
+cp podinfo-with-redis/gen-man-by-dev-podinfo-redis.yaml apps/gen-man-by-dev-podinfo.yaml
+pe "echo \"GitOps Deployment - Demo 2\""
+./scripts/sync_argocd_apps.sh demo-2 >/dev/null 2>&1""
+pe "kubectl get workload,all,hpa,netpol,sa -n podinfo-kro"
 pe "echo \"What about Redis outside of the cluster, hosted by a Cloud Provider for example?\""
 pe "code images/demo-2-redis-in-gcp.png"
+pe "code -g kro/workload.yaml:421"
+pe "code podinfo-with-redis/gen-man-by-dev-podinfo-minimal-setup-gcp.yaml"
 
 # Demo #3
 pe "echo \"Demo #3 - Extend Abstraction with Flexibility with Kro & Score\""
 pe "code images/demo-2.png"
+pe "code podinfo-with-redis/score.yaml"
 pe "score-k8s init \
     --no-sample \
     --no-default-provisioners \
     --patch-templates https://raw.githubusercontent.com/score-spec/community-patchers/refs/heads/main/score-k8s/namespace-pss-restricted.tpl \
     --patch-templates ./score-k8s/kro-workload-patch-template.tpl \
     --provisioners ./score-k8s/kro-provisioners.yaml"
+pe "code podinfo-with-redis/score-minimal.yaml"
 pe "score-k8s generate podinfo/score.yaml \
     --image ghcr.io/stefanprodan/podinfo:6.9.2 \
     --namespace podinfo-score \
     --generate-namespace \
-    --override-property containers.podinfo.variables.PODINFO_UI_MESSAGE="Hello, from ArgoCD, Kro and Score" \
+    --override-property containers.podinfo.variables.PODINFO_UI_MESSAGE=\"Hello, from ArgoCD, Kro and Score\" \
     --output apps/gen-by-score-podinfo.yaml"
-# pe "./scripts/sync_argocd_apps.sh demo-3"
+pe "./scripts/sync_argocd_apps.sh demo-3"
 
 # Demo #4
 pe "echo \"Demo #4 - Bridging Inner and Outer Loops - Score with Docker Compose\""
